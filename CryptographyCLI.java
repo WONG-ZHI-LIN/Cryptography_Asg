1552e57a2150928077e10d40fd5bb6091900fd72
bellwong <1201203299@student.mmu.edu.my>
2025-02-18 18:48:35 +0800

    remove comment

--- CryptographyCLI.java
+++ CryptographyCLI.java
@@ -491,7 +491,6 @@ class ManualRSAEncryption {
         }
     }
 
-    // Change from private to public to allow calling from main()
     public static void performHybridEncryption(java.util.Scanner scanner) throws Exception {
         System.out.println("\n======================================================");
         System.out.println(" SIMULATING PERSON A & B COMMUNICATION USING RSA & AES (CBC) ");
